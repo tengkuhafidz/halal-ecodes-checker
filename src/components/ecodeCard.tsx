2820eeed8a17a6e6ce9bcfa96763b781325d864d
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2020-09-18 17:45:55 +0800

    create bottomCta component (but dont render)

--- src/components/ecodeCard.tsx
+++ src/components/ecodeCard.tsx
@@ -6,19 +6,10 @@ interface Props {
 }
 
 const EcodeCard: React.FC<Props> = ({ ecodeData: { ecode, halalStatus, category, whatIsIt, chemicalName }}) => (
-  <div className={`grid-col-1 rounded-lg p-8  text-white ${ halalStatus === "Halal" ? "bg-green-600" : "bg-red-600"}`}>
+  <div className={`grid-col-1 rounded-lg p-8  text-white shadow-lg hover:shadow-2xl ${ halalStatus === "Halal" ? "bg-green-600" : "bg-red-600"}`}>
     <h2 className="text-xl font-bold">e{ecode} - {halalStatus}</h2>
     <p className="mt-2 font-light">{chemicalName}</p>
     <p className="mt-4 font-light">{category}: {whatIsIt}</p>
-    <p className="mt-4 font-light">Is this web app helpful? Buy a coffee for the developer or share it around!</p>
-    <div className="mt-4 text-sm">
-      <button className=" mr-2 bg-white text-gray-800 hover:bg-gray-200 py-2 px-4 rounded">
-        Buy Coffee
-      </button>
-      <button className="bg-white text-gray-800 hover:bg-gray-200 py-2 px-4 rounded">
-        Share
-      </button>
-    </div>
   </div>
 )
 
