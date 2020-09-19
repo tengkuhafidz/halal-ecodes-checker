daff3f93b78c6afcdcf62d760266ce59bd463e79
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2020-09-19 12:54:51 +0800

    enhanced padding

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ const Home = ({ data }) => {
   return (
     <div className="bg-gray-100 min-h-screen text-gray-800">
       <SEO />
-      <div className="max-w-6xl mx-auto py-12 px-4">
+      <div className="max-w-6xl mx-auto pt-12 pb-4 px-4">
         <h1 className="text-4xl font-semibold text-center">Additive Halal Check</h1>
         <Main ecodesData={ecodesData} />
         <Footer />
