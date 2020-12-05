03b0be7727a0b16ce29503282b29e463af2b2db8
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2020-12-05 08:38:10 +0800

    add feedback fish

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,18 +1,24 @@
 import React from 'react'
+import { FeedbackFish } from '@feedback-fish/react'
 
 const Footer: React.FC = () => (
-  <p className="text-center text-gray-600 mt-24">
-    Created by{' '}
-    <a
-      href="https://buymeacoffee.com/tengkubros"
-      target="_blank"
-      rel="noreferrer"
-      className="underline hover:text-gray-800"
-    >
-      Tengku Bros
-    </a>
-    , in Sunny Singapore 🇸🇬
-  </p>
+  <div className="text-center text-gray-600 mt-24">
+    <FeedbackFish projectId="ec36509458fc48">
+      <button className="underline mb-4">Have a feeback? Let us know!</button>
+    </FeedbackFish>
+    <p>
+      Created by{' '}
+      <a
+        href="https://buymeacoffee.com/tengkubros"
+        target="_blank"
+        rel="noreferrer"
+        className="underline hover:text-gray-800"
+      >
+        Tengku Bros
+      </a>
+      , in Sunny Singapore 🇸🇬
+    </p>
+  </div>
 )
 
 export default Footer
